eb7f4968d6b4c2811a1ae7e0e081a665646a2401
nick <niclewis@uw.edu>
2019-02-24 20:14:45 -0800

    magical item

--- sql_statements/character.sql
+++ sql_statements/character.sql
@@ -6,4 +6,9 @@ CREATE TABLE character(
   background VARCHAR(30),
   level INT CHECK(level >= 0 AND level <=20),
   PRIMARY KEY(dci_number, name)
-)
+);
+
+INSERT INTO character
+VALUES ('1234', 'fred', 'human', 'archer', 'none', 10);
+
+SELECT * FROM character;
---
+++ sql_statements/magical_item.sql
@@ -0,0 +1,9 @@
+CREATE TABLE magical_item(
+  dci_number INT NOT NULL,
+  character_name VARCHAR(30),
+  name VARCHAR(30),
+  quantity VARCHAR(30),
+  date_acquired INT,
+  PRIMARY KEY (dci_number, character_name, name)
+);
+
