cd919a9ba3f51b027d64869ad38b07d295e157b9
Cody Stuck <hellocodystuck@gmail.com>
2019-03-08 14:04:44 -0800

    added test_data_.sql

---
+++ sql_statements/test_data.sql
@@ -0,0 +1,51 @@
+/*player 1*/
+insert into PLAYER values (1422314756,'Arthur A.');
+
+insert into P_CHARACTER values (1422314756,'Aelar','Wizard','Sage','High Elf',1);
+
+insert into MAGICAL_ITEM values (1422314756,'Aelar','Cloak Of Protection',2016-3-16);
+insert into MAGICAL_ITEM values (1422314756,'Aelar','Wand of Magic Missiles',2017-4-18);
+
+insert into ADVENTURE_LOG_ENTRY values (1,1422314756,'Aelar','Storm Kings Thunder',2016-3-16,5,2,0,0,0,10,2);
+insert into ADVENTURE_LOG_ENTRY values (2,1422314756,'Aelar','Out Of The Abyss',2017-4-18,5,2,0,0,0,10,2);
+
+insert into DOWNTIME_LOG_ENTRY values (1,1422314756,'Aelar',2016-3-17,0,0,0,0,-5,0,0,-5);
+insert into DOWNTIME_LOG_ENTRY values (1,1422314756,'Aelar',2017-4-18,0,0,0,0,-10,0,0,-10);
+
+insert into P_CHARACTER values (1422314756,'Garreth','Cleric','Acolyte','Human',2);
+
+insert into MAGICAL_ITEM values (1422314756,'Garreth','+1 Shield',2017-5-12);
+insert into MAGICAL_ITEM values (1422314756,'Garreth','Periapt Of Wound Closure',2018-8-2);
+
+insert into ADVENTURE_LOG_ENTRY values (1,1422314756,'Garreth','Tales From The Yawning Portal',2017-5-12,5,2,0,0,0,10,2);
+insert into ADVENTURE_LOG_ENTRY values (2,1422314756,'Garreth','Tomb Of Annihilation',2018-8-2,5,2,0,0,0,10,2);
+
+insert into DOWNTIME_LOG_ENTRY values (1,1422314756,'Garreth',2017-5-12,0,0,0,0,-5,0,0,-5);
+insert into DOWNTIME_LOG_ENTRY values (1,1422314756,'Garreth',2018-8-2,0,0,0,0,-10,0,0,-10);
+
+/*player 2*/
+insert into PLAYER values (2521489631,'Bartholomew B.');
+
+insert into P_CHARACTER values (1422314756,'Thorun','Wizard','Sage','High Elf',1);
+
+insert into MAGICAL_ITEM values (1422314756,'Thorun','+1 Warhammer',2017-6-23);
+insert into MAGICAL_ITEM values (1422314756,'Thorun','Bag Of Holding',2017-8-10);
+
+insert into ADVENTURE_LOG_ENTRY values (1,1422314756,'Thorun','Dragon Heist',2017-6-23,5,2,0,0,0,10,2);
+insert into ADVENTURE_LOG_ENTRY values (2,1422314756,'Thorun','Tales From The Yawning Portal',2017-8-10,5,2,0,0,0,10,2);
+
+insert into DOWNTIME_LOG_ENTRY values (1,1422314756,'Thorun',2016-3-17,0,0,0,0,-5,0,0,-5);
+insert into DOWNTIME_LOG_ENTRY values (1,1422314756,'Thorun',2017-8-10,0,0,0,0,-10,0,0,-10);
+
+insert into P_CHARACTER values (1422314756,'Dorn','Cleric','Acolyte','Human',2);
+
+insert into MAGICAL_ITEM values (1422314756,'Dorn','+1 Greatsword',2018-1-23);
+insert into MAGICAL_ITEM values (1422314756,'Dorn','Bag Of Tricks',2018-4-22);
+
+insert into ADVENTURE_LOG_ENTRY values (1,1422314756,'Dorn','Curse Of Strahd',2018-1-23,5,2,0,0,0,10,2);
+insert into ADVENTURE_LOG_ENTRY values (2,1422314756,'Dorn','Princes Of The Apocolypse',2018-4-22,5,2,0,0,0,10,2);
+
+insert into DOWNTIME_LOG_ENTRY values (1,1422314756,'Dorn',2018-1-23,0,0,0,0,-5,0,0,-5);
+insert into DOWNTIME_LOG_ENTRY values (1,1422314756,'Dorn',2018-4-22,0,0,0,0,-10,0,0,-10);
+
+
