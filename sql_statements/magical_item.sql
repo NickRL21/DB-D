1a8f194804e63e90540e03e69f1063ee6d8e5758
zihansun <34561248+zihansun@users.noreply.github.com>
2019-03-08 16:31:37 -0800

    Update magical_item.sql

--- sql_statements/magical_item.sql
+++ sql_statements/magical_item.sql
@@ -3,7 +3,7 @@ CREATE TABLE Magical_item(
   character_name VARCHAR(30),
   name VARCHAR(30),
   quantity VARCHAR(30),
-  date_acquired INT,
+  date_acquired DATE,
   PRIMARY KEY (dci_number, character_name, name)
 );
 
